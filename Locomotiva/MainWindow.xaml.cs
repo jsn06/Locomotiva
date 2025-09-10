d3db10413171afa97be6a0656745b07df33cea04
jsn06 <jvsouzaan@gmail.com>
2025-09-10 14:46:52 -0300

    final 2

--- Locomotiva/MainWindow.xaml.cs
+++ Locomotiva/MainWindow.xaml.cs
@@ -9,122 +9,115 @@ namespace Locomotiva
 {
     public partial class MainWindow : Window
     {
-        private RotateTransform _roda1Rotacao;
-        private RotateTransform _roda2Rotacao;
-        private RotateTransform _roda3Rotacao;
-        private RotateTransform _bielaRotacao;
-        private TranslateTransform _bielaTranslacao;
+        private RotateTransform rot1, rot2, rot3, rotBiela;
+        private TranslateTransform movBiela;
 
-        private double _raioRoda = 30;
-        private double _centroRoda1X = 150;
-        private double _centroRoda1Y = 200;
-        private double _centroRoda2X = 250;
-        private double _centroRoda2Y = 200;
-        private double _centroRoda3X = 350;
-        private double _centroRoda3Y = 200;
-        private double _comprimentoBiela;
+        private double raio = 30;
+        private double x1 = 150, y1 = 200;
+        private double x2 = 250, y2 = 200;
+        private double x3 = 350, y3 = 200;
+        private double tamBiela;
 
-        private DispatcherTimer _temporizador;
-        private DateTime _inicio;
+        private DispatcherTimer timer;
+        private DateTime inicio;
 
-        private DoubleAnimation _animacaoLocomotiva;
-        private double _ultimaPosXLocomotiva = 0;
-        private int _direcao = 1; // 1 = direita, -1 = esquerda
-        private double _anguloRoda = 0;
+        private DoubleAnimation animacao;
+        private double ultX = 0;
+        private int direcao = 1; // 1 = direita, -1 = esquerda
+        private double angulo = 0;
 
         public MainWindow()
         {
             InitializeComponent();
-            Loaded += MainWindow_Carregado;
-            SizeChanged += MainWindow_TamanhoAlterado;
+            Loaded += JanelaCarregada;
+            SizeChanged += TamanhoMudou;
         }
 
-        private void MainWindow_Carregado(object sender, RoutedEventArgs e)
+        private void JanelaCarregada(object sender, RoutedEventArgs e)
         {
-            _roda1Rotacao = (RotateTransform)Wheel1.Template.FindName("WheelRotate", Wheel1);
-            _roda2Rotacao = (RotateTransform)Wheel2.Template.FindName("WheelRotate", Wheel2);
-            _roda3Rotacao = (RotateTransform)Wheel3.Template.FindName("WheelRotate", Wheel3);
+            rot1 = (RotateTransform)roda1.Template.FindName("RotacaoRoda", roda1);
+            rot2 = (RotateTransform)roda2.Template.FindName("RotacaoRoda", roda2);
+            rot3 = (RotateTransform)roda3.Template.FindName("RotacaoRoda", roda3);
 
-            var grupoTransformacao = (TransformGroup)Biela.RenderTransform;
-            _bielaRotacao = (RotateTransform)grupoTransformacao.Children[0];
-            _bielaTranslacao = (TranslateTransform)grupoTransformacao.Children[1];
+            var grupo = (TransformGroup)biela.RenderTransform;
+            rotBiela = (RotateTransform)grupo.Children[0];
+            movBiela = (TranslateTransform)grupo.Children[1];
 
-            _comprimentoBiela = Math.Sqrt(Math.Pow(_centroRoda3X - _centroRoda1X, 2) + Math.Pow(_centroRoda3Y - _centroRoda1Y, 2));
+            tamBiela = Math.Sqrt(Math.Pow(x3 - x1, 2) + Math.Pow(y3 - y1, 2));
 
-            _inicio = DateTime.Now;
-            _temporizador = new DispatcherTimer { Interval = TimeSpan.FromMilliseconds(16) };
-            _temporizador.Tick += AtualizarAnimacao;
-            _temporizador.Start();
+            inicio = DateTime.Now;
+            timer = new DispatcherTimer { Interval = TimeSpan.FromMilliseconds(16) };
+            timer.Tick += Atualiza;
+            timer.Start();
 
-            IniciarAnimacaoLocomotiva();
+            IniciaAnimacao();
         }
 
-        private void AtualizarAnimacao(object sender, EventArgs e)
+        private void Atualiza(object sender, EventArgs e)
         {
-            // Detecta direção do movimento
-            double posXAtual = LocomotiveTranslate.X;
-            if (posXAtual > _ultimaPosXLocomotiva + 0.1)
-                _direcao = 1; // Direita
-            else if (posXAtual < _ultimaPosXLocomotiva - 0.1)
-                _direcao = -1; // Esquerda
+            // Descobre se está indo pra direita ou esquerda
+            double xAtual = movLocomotiva.X;
+            if (xAtual > ultX + 0.1)
+                direcao = 1;
+            else if (xAtual < ultX - 0.1)
+                direcao = -1;
 
-            _ultimaPosXLocomotiva = posXAtual;
+            ultX = xAtual;
 
-            // Atualiza ângulo das rodas conforme direção
-            double tempoDecorrido = (DateTime.Now - _inicio).TotalSeconds;
-            double velocidade = 240; // graus por segundo
-            _anguloRoda += _direcao * velocidade * _temporizador.Interval.TotalSeconds;
-            _anguloRoda %= 360;
+            // Gira as rodas
+            double vel = 240;
+            angulo += direcao * vel * timer.Interval.TotalSeconds;
+            angulo %= 360;
 
-            _roda1Rotacao.Angle = _anguloRoda;
-            _roda2Rotacao.Angle = _anguloRoda;
-            _roda3Rotacao.Angle = _anguloRoda;
+            rot1.Angle = angulo;
+            rot2.Angle = angulo;
+            rot3.Angle = angulo;
 
-            // Atualiza biela (usando o ângulo atual)
-            double anguloRad = _anguloRoda * Math.PI / 180.0;
-            double deslocamentoBiela = 20;
-            double x1 = _centroRoda1X + deslocamentoBiela * Math.Cos(anguloRad);
-            double y1 = _centroRoda1Y + deslocamentoBiela * Math.Sin(anguloRad);
-            double x3 = _centroRoda3X + deslocamentoBiela * Math.Cos(anguloRad);
-            double y3 = _centroRoda3Y + deslocamentoBiela * Math.Sin(anguloRad);
+            // Move a biela junto
+            double rad = angulo * Math.PI / 180.0;
+            double off = 20;
+            double bx1 = x1 + off * Math.Cos(rad);
+            double by1 = y1 + off * Math.Sin(rad);
+            double bx3 = x3 + off * Math.Cos(rad);
+            double by3 = y3 + off * Math.Sin(rad);
 
-            double dx = x3 - x1;
-            double dy = y3 - y1;
-            double anguloBiela = Math.Atan2(dy, dx) * 180.0 / Math.PI;
+            double dx = bx3 - bx1;
+            double dy = by3 - by1;
+            double angBiela = Math.Atan2(dy, dx) * 180.0 / Math.PI;
 
-            _bielaTranslacao.X = x1 - 150;
-            _bielaTranslacao.Y = y1 - 200;
-            _bielaRotacao.Angle = anguloBiela;
+            movBiela.X = bx1 - 150;
+            movBiela.Y = by1 - 200;
+            rotBiela.Angle = angBiela;
         }
 
-        private void MainWindow_TamanhoAlterado(object sender, SizeChangedEventArgs e)
+        private void TamanhoMudou(object sender, SizeChangedEventArgs e)
         {
-            IniciarAnimacaoLocomotiva();
+            IniciaAnimacao();
         }
 
-        private void IniciarAnimacaoLocomotiva()
+        private void IniciaAnimacao()
         {
-            LocomotiveTranslate.BeginAnimation(TranslateTransform.XProperty, null);
+            movLocomotiva.BeginAnimation(TranslateTransform.XProperty, null);
 
-            double larguraContainer = 0;
+            double largura = 0;
             if (this.Content is Grid grid)
-                larguraContainer = grid.ActualWidth;
+                largura = grid.ActualWidth;
             else
-                larguraContainer = this.ActualWidth;
+                largura = this.ActualWidth;
 
-            double larguraLocomotiva = LocomotiveCanvas.ActualWidth > 0 ? LocomotiveCanvas.ActualWidth : LocomotiveCanvas.Width;
-            double maxX = Math.Max(0, larguraContainer - larguraLocomotiva);
+            double larguraLocomotiva = tela.ActualWidth > 0 ? tela.ActualWidth : tela.Width;
+            double maxX = Math.Max(0, largura - larguraLocomotiva);
 
             if (maxX <= 0)
             {
-                LocomotiveTranslate.X = 0;
+                movLocomotiva.X = 0;
                 return;
             }
 
             double segundos = Math.Max(2.0, maxX / 100.0);
             var duracao = new Duration(TimeSpan.FromSeconds(segundos));
 
-            _animacaoLocomotiva = new DoubleAnimation
+            animacao = new DoubleAnimation
             {
                 From = 0,
                 To = maxX,
@@ -133,8 +126,8 @@ namespace Locomotiva
                 RepeatBehavior = RepeatBehavior.Forever
             };
 
-            LocomotiveTranslate.X = 0;
-            LocomotiveTranslate.BeginAnimation(TranslateTransform.XProperty, _animacaoLocomotiva);
+            movLocomotiva.X = 0;
+            movLocomotiva.BeginAnimation(TranslateTransform.XProperty, animacao);
         }
     }
 }
